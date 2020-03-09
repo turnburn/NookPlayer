af25ce553b8218ad7230a49b28f09c1a606d9a0a
Brandon Turner <bcturner@gmail.com>
2020-03-08 21:15:12 -0400

    Added Time Functionality

--- src/index.js
+++ src/index.js
@@ -4,27 +4,56 @@ var howler = require('howler');
 
 const BrowserWindow = electron.remote.BrowserWindow
 
-const wildworldBtn = document.getElementById('wildworldBtn')
-const newleafBtn = document.getElementById('newleafBtn')
+//Games available
+const games = {
+    WILDWORLD: 'ww',
+    NEWLEAF: 'nl'
+}
 
+//Initialize the first game to be Wild World
+var game = games.WILDWORLD
 var player = new howler.Howl({
   src: ['../assets/audio/ww/ww-1-am.mp3']
 });
 
-wildworldBtn.addEventListener('click', function(event) {
-	player.stop()
-	player = new howler.Howl({
-	  src: ['../assets/audio/ww/ww-1-am.mp3']
-	});
+//Buttons 
+const wildworldBtn = document.getElementById('wildworldBtn')
+const newleafBtn = document.getElementById('newleafBtn')
 
-	player.play();
+wildworldBtn.addEventListener('click', function(event) {
+	game = games.WILDWORLD
+	playFile();
 })
 
 newleafBtn.addEventListener('click', function(event) {
+	game = games.NEWLEAF
+	playFile();
+})
+
+function playFile(){
+	//Stop playing the current song
 	player.stop()
-	player = new howler.Howl({
-	  src: ['../assets/audio/nl/nl-1-am.mp3']
+	//Find the time of day
+	var hour = new Date().getHours();
+
+	let mp3File = '../assets/audio/';
+
+	if(hour > 12){
+  		mp3File = mp3File.concat(game,'/',game,'-', (hour - 12).toString() ,'-pm.mp3');
+  	}
+  	else{
+		mp3File = mp3File.concat(game,'/',game,'-', hour.toString() ,'-am.mp3');
+  	}
+
+  	console.log("Running playFile with "+mp3File)
+
+    player = new howler.Howl({
+	  src: [mp3File]
 	});
 
 	player.play();
-})
+
+	player.on('end', function(){
+  		playFile();
+	});
+}
